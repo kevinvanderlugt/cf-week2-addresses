da25363778b419e810a40dd6b77524d0302143ae
Kevin VanderLugt <kevin.vanderlugt@gmail.com>
2013-08-18 20:46:39 -0700

    Adding the ability to add an age to a person in the contact list

--- contacts.rb
+++ contacts.rb
@@ -4,6 +4,7 @@ class Person
   attr_accessor :first_name
   attr_accessor :last_name
   attr_accessor :phone
+  attr_accessor :age
 
   def full_name
     first_name + " " + last_name
@@ -11,6 +12,10 @@ class Person
 end
 
 class Contacts
+  NAME_JUSTIFY  = 30
+  PHONE_JUSTIFY = 12
+  AGE_JUSTIFY   = 8
+
   def self.run
     people = []
 
@@ -18,10 +23,14 @@ class Contacts
       command = ask("> ")
 
       if command == "list"
-        puts "Name".ljust(30) + "Phone".rjust(12)
-        puts "-" * 42
+        puts "Name".ljust(NAME_JUSTIFY) + 
+              "Phone".rjust(PHONE_JUSTIFY) + 
+              "Age".rjust(AGE_JUSTIFY)
+        puts "-" * (NAME_JUSTIFY + PHONE_JUSTIFY + AGE_JUSTIFY)
         people.each do |person|
-          puts person.full_name.ljust(30) + person.phone.rjust(12)
+          puts person.full_name.ljust(NAME_JUSTIFY) + 
+                person.phone.rjust(PHONE_JUSTIFY) + 
+                person.age.rjust(AGE_JUSTIFY)
         end
 
       elsif command == "add"
@@ -30,6 +39,7 @@ class Contacts
         person.first_name = ask("First Name: ")
         person.last_name  = ask("Last Name: ")
         person.phone      = ask("Phone: ")
+        person.age        = ask("Age: ")
 
         people.push(person)
       elsif command == "quit"
